ab7fbd074aae80fbc88f338e611d34bd4646c72e
jfrazier <johnfrazier1222@gmail.com>
2018-07-11 17:20:14 -0400

    completely forgot to push this, did the html layout and styles. also made a few functions

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function signUpPopUp(){
+  document.getElementById('popup').classList.remove('hide');
+};
+
+function cancelSignUp(){
+  document.getElementById('popup').classList.add('hide');
+  alert('Maybe another time')
+};
+
+function signUp(){
+  document.getElementById('popup').classList.add('hide');
+  alert('Thank you for signing up!!!');
+}
+
